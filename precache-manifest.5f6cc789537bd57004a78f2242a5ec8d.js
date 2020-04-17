43b676cb6dfb2e4d1ebb05c71e5fc8ae08073897
Harry <harrynegus@hotmail.co.uk>
2020-04-17 22:32:02 +0100

    Updates

---
+++ precache-manifest.5f6cc789537bd57004a78f2242a5ec8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a54717091a6a83be97989337b8539e3",
+    "url": "/slacks-or-deep-slacks/index.html"
+  },
+  {
+    "revision": "797c9ccbff4305809e00",
+    "url": "/slacks-or-deep-slacks/static/css/2.093d98ae.chunk.css"
+  },
+  {
+    "revision": "797c9ccbff4305809e00",
+    "url": "/slacks-or-deep-slacks/static/js/2.52d17a3d.chunk.js"
+  },
+  {
+    "revision": "14a886f7ea49631b50a1269c66ff1ee1",
+    "url": "/slacks-or-deep-slacks/static/js/2.52d17a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b86be41f4539075990",
+    "url": "/slacks-or-deep-slacks/static/js/main.688eb275.chunk.js"
+  },
+  {
+    "revision": "a5543c03afce24e9d608",
+    "url": "/slacks-or-deep-slacks/static/js/runtime-main.f54acc00.js"
+  },
+  {
+    "revision": "6a7176fb259aafb9ad4540c3fb151074",
+    "url": "/slacks-or-deep-slacks/static/media/question.6a7176fb.png"
+  }
+]);
